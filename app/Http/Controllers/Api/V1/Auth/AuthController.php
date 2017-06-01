6035932016ff3cdbf1145635c359dc2c8c4e47ab
ratul <ratulcse27@gmail.com>
2017-06-01 18:33:13 +0600

    move auth to auth service

--- app/Http/Controllers/Api/V1/Auth/AuthController.php
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -6,8 +6,7 @@ use App\Responses\ApiResponse;
 use App\Services\Auth\AuthService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Tymon\JWTAuth\Exceptions\JWTException;
-use Tymon\JWTAuth\Facades\JWTAuth;
+
 
 class AuthController extends Controller
 {
@@ -38,20 +37,8 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-
-        $credentials = $request->only('email', 'password');
-
-        try {
-            // attempt to verify the credentials and create a token for the user
-            if (! $token = JWTAuth::attempt($credentials)) {
-                return $this->apiResponse->error("invalid credentials");
-            }
-        } catch (JWTException $e) {
-            // something went wrong whilst attempting to encode the token
-            return $this->apiResponse->error("could not create token");
-        }
-
-        return $this->apiResponse->success($token);
+        $response = $this->authService->login($request);
+        return $response;
     }
 
     public function refreshToken()
--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -9,13 +9,48 @@
 namespace App\Services\Auth;
 
 
+use App\Responses\ApiResponse;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
 class AuthService
 {
+    /**
+     * @var UserService
+     */
+    private $userService;
+    /**
+     * @var ApiResponse
+     */
+    private $apiResponse;
 
     /**
      * AuthService constructor.
+     * @param UserService $userService
+     * @param ApiResponse $apiResponse
      */
-    public function __construct()
+    public function __construct(UserService $userService,ApiResponse $apiResponse)
     {
+        $this->userService = $userService;
+        $this->apiResponse = $apiResponse;
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return $this->apiResponse->error("invalid credentials");
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return $this->apiResponse->error("could not create token");
+        }
+
+        return $this->apiResponse->success($token);
     }
 }
